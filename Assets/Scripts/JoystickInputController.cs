6fa6561b3dec8e265f4f902e34a7f4d26de3470d
Sahapat <sahapat.starcaft@gmail.com>
2018-05-14 17:27:44 +0700

    Map V1.0

---
+++ Assets/Scripts/JoystickInputController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+public class JoystickInputController : MonoBehaviour
+{
+    public static Vector2 getMovement(byte playerId,bool isJoy)
+    {
+        float x = 0;
+        float y = 0;
+        Vector2 movement = Vector2.zero;
+        if(isJoy)
+        {
+            x = Input.GetAxisRaw("Horizontal" + playerId);
+            y = Input.GetAxisRaw("Vertical" + playerId);
+        }
+        else
+        {
+            switch(playerId)
+            {
+                case 1:
+                    if(Input.GetKey(KeyCode.A))
+                    {
+                        x = -1;
+                    }
+                    else if(Input.GetKey(KeyCode.D))
+                    {
+                        x = 1;
+                    }
+
+                    if (Input.GetKey(KeyCode.W))
+                    {
+                        y = 1;
+                    }
+                    else if (Input.GetKey(KeyCode.S))
+                    {
+                        y = -1;
+                    }
+                    break;
+                case 2:
+                    if (Input.GetKey(KeyCode.LeftArrow))
+                    {
+                        x = -1;
+                    }
+                    else if (Input.GetKey(KeyCode.RightArrow))
+                    {
+                        x = 1;
+                    }
+
+                    if (Input.GetKey(KeyCode.UpArrow))
+                    {
+                        y = 1;
+                    }
+                    else if (Input.GetKey(KeyCode.DownArrow))
+                    {
+                        y = -1;
+                    }
+                    break;
+            }
+        }
+        movement = new Vector2(x, y);
+        return movement;
+    }
+}
