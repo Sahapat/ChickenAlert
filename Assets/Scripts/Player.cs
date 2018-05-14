9753b23820d485c41fb2bc376e90327f9360b747
Sahapat <sahapat.starcaft@gmail.com>
2018-05-14 12:44:38 +0700

    Add Project

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private GameObject agentParent;
+    [SerializeField] private float speed = 5f;
+    private bool isGround;
+    private void FixedUpdate()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveZ = Input.GetAxisRaw("Vertical");
+        Movement(moveX,moveZ);
+    }
+    private void Movement(float moveX,float moveZ)
+    {
+        agentParent.transform.Translate(new Vector3(moveX * speed * Time.deltaTime, 0, moveZ * speed * Time.deltaTime));
+    }
+}
